7377db62206b92af8334cceb2cdc1ea449e01932
Kas <31658826+Kassandraeam@users.noreply.github.com>
2022-07-12 17:31:09 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
 $(document).ready(() => {
+  
+  $('#text').on('keyup', (event) => {
+    $('.preview').html($(event.currentTarget).val())
+  });
 
-})
+  $('#font').on('change', (event) => {
+    $('.preview').css({
+      fontFamily: $(event.currentTarget).val() //remember that css takes an Object and takes in camelBack fonts. Not .preview or whatevaa. And don't end val() with a semicolon it'll break it.
+    })
+  });
+
+  $('#weight').on('change', (event) => {
+    $('.preview').css({
+      fontWeight: $(event.currentTarget).val()
+    })
+  });
+
+  $('#size').on('keyup', (event) => {
+      let fontSize = $(event.currentTarget).val() + 'px';
+      $('.preview').css({
+        fontSize: fontSize
+      })
+  })
+
+});
